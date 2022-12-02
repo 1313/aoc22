c5892e507aa9b64dde61d26d974056ecd282778b
Per Johansson <per.johansson.1@svt.se>
2022-12-02 11:47:27 +0100

    02

---
+++ 02.mjs
@@ -0,0 +1,78 @@
+const sum = (a) => a.reduce((a, b) => a + b, 0);
+
+function draw(symbol) {
+  switch (symbol) {
+    case "A":
+    case "X":
+      return 1;
+    case "B":
+    case "Y":
+      return 2;
+    case "C":
+    case "Z":
+      return 3;
+  }
+}
+
+function pick([me, opponent]) {
+  switch (me) {
+    case 1:
+      switch (opponent) {
+        case 1:
+          return 3;
+        case 2:
+          return 1;
+        case 3:
+          return 2;
+      }
+    case 2:
+      switch (opponent) {
+        case 1:
+          return 1;
+        case 2:
+          return 2;
+        case 3:
+          return 3;
+      }
+    case 3:
+      switch (opponent) {
+        case 1:
+          return 2;
+        case 2:
+          return 3;
+        case 3:
+          return 1;
+      }
+  }
+  throw Error("");
+}
+
+function game(draws) {
+  const scores = draws.split(" ").map(draw);
+  //v1
+  //   const me = scores[1];
+
+  // v2
+  const me = pick(scores);
+  const opponent = scores[0];
+
+  const diff = Math.abs(me - opponent);
+  const isDraw = diff === 0;
+  const isWin =
+    diff === 1 ? Math.max(me, opponent) === me : Math.min(me, opponent) === me;
+
+  if (isDraw) {
+    return me + 3;
+  } else if (isWin) {
+    return me + 6;
+  } else {
+    return me;
+  }
+}
+
+import("fs")
+  .then((fs) => fs.promises.readFile("./input.txt"))
+  .then((b) => b.toString().trim())
+  .then((s) => s.split("\n").map(game))
+  .then(sum)
+  .then(console.log);
